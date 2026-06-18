05893d0874a099ac7a9dac1b0d2748620832ba99
ineocases <zumagamingarg@gmail.com>
2026-06-18 20:30:06 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,34 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnMXBl-WIagKp_sSeDZUHxJdUhdSs-AaE",
-  authDomain: "ineocases.firebaseapp.com",
-  projectId: "ineocases",
-  storageBucket: "ineocases.firebasestorage.app",
-  messagingSenderId: "904152906036",
-  appId: "1:904152906036:web:85e24721ef7cddbc230d61"
+
+apiKey:"AIzaSyAnMXBl-WIagKp_sSeDZUHxJdUhdSs-AaE",
+
+authDomain:"ineocases.firebaseapp.com",
+
+projectId:"ineocases",
+
+storageBucket:"ineocases.firebasestorage.app",
+
+messagingSenderId:"904152906036",
+
+appId:"1:904152906036:web:85e24721ef7cddbc230d61"
+
 };
+
+
+
 const app = initializeApp(firebaseConfig);
 
+
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
+
